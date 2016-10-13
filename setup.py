a90212029b93442607f335c908d9efc18a630aef
Masashi Yoshikawa <yoshikawa.masashi.yh8@is.naist.jp>
2016-10-14 03:38:11 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,26 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 ext_modules = [
         Extension("utils",
                   ["utils.pyx"],
+                  include_dirs=[numpy.get_include()]
                   ),
         # Extension("astar",
         #           ["astar.pyx"],
         #           language="c++",
         #           libraries=["m"]
         #           ),
-        Extension("pqueue",
-                  ["pqueue.pyx"],
-                  ),
+        # Extension("pqueue",
+        #           ["pqueue.pyx"],
+        #           ),
         ]
 
 setup(
         name = "ccg parser",
         cmdclass = { "build_ext" : build_ext },
         ext_modules = ext_modules,
+        include_dirs=[numpy.get_include()]
 )
