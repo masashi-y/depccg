7b0c91967178e99652529d2664c565d299218c51
Masashi Yoshikawa <yoshikawa.masashi.yh8@is.naist.jp>
2016-11-29 01:44:32 +0900

    cleanup

--- cpp/tagger.pyx
+++ cpp/tagger.pyx
@@ -15,13 +15,8 @@ cdef public void tag(const char* model_path, const char* c_str, int length, floa
     chainer.serializers.load_npz(model, tagger)
     cdef np.ndarray[np.float32_t, ndim=1] res = tagger.predict(py_str).flatten()
     cdef int i, j
-    # cdef int nword = res.shape[0]
-    # cdef int ntag = res.shape[1]
 
     memcpy(out, res.data, res.shape[0] * sizeof(float))
-    # for i in xrange(nword):
-        # out[i] = res[i]
-        # for j in xrange(ntag):
 
 
 cdef public void tag_doc(const char* model_path, const char** c_strs, int* lengths, int doc_size, float** outs):
@@ -38,7 +33,7 @@ cdef public void tag_doc(const char* model_path, const char** c_strs, int* lengt
     cdef list inputs = []
     for i in xrange(doc_size):
         inputs.append(
-                c_strs[i][:lengths[i]].decode("utf-8").encode("utf-8").split(" "))
+                c_strs[i][:lengths[i]].decode("utf-8").split(" "))
 
     res = tagger.predict_doc(inputs)
     for i, _, scores in res:
