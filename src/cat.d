320f7f452a19b81d32700e50e6947a493a537487
Masashi Yoshikawa <yoshikawa.masashi.yh8@is.naist.jp>
2016-12-13 20:10:29 +0900

    update parser

--- src/cat.d
+++
@@ -1,9 +0,0 @@
-cat.o: cat.cpp cat.h cacheable.h feat.h utils.h
-
-cat.h:
-
-cacheable.h:
-
-feat.h:
-
-utils.h:
--- src/utils.d
+++
@@ -1,9 +0,0 @@
-utils.o: utils.cpp utils.h cat.h cacheable.h feat.h
-
-utils.h:
-
-cat.h:
-
-cacheable.h:
-
-feat.h:
