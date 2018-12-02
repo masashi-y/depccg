5e7204bac6edbc6694a990ef224451666f0b6c71
Masashi Yoshikawa <yoshikawa.masashi.yh8@is.naist.jp>
2018-12-02 21:57:57 +0900

    update

---
+++ depccg/parser.pxd
@@ -0,0 +1,70 @@
+from libcpp.vector cimport vector
+from libcpp.string cimport string
+from libcpp.unordered_set cimport unordered_set
+from libcpp.unordered_map cimport unordered_map
+from libcpp cimport bool
+from .cat cimport Category, Cat, CatPair
+from .tree cimport Tree, ScoredNode
+from .combinator cimport Op
+
+
+cdef extern from "depccg.h" namespace "myccg" nogil:
+    cdef cppclass RuleCache
+
+    cdef cppclass AgendaItem
+
+    ctypedef vector[RuleCache]& (*ApplyBinaryRules)(
+            unordered_map[CatPair, vector[RuleCache]]&,
+            const vector[Op]&, const unordered_set[CatPair]&, Cat, Cat)
+
+    ctypedef vector[Cat] (*ApplyUnaryRules)(
+            const unordered_map[Cat, vector[Cat]]&, NodeType)
+
+    ApplyUnaryRules EnApplyUnaryRules
+
+    ApplyUnaryRules JaApplyUnaryRules
+
+    ApplyBinaryRules EnGetRules
+
+    ApplyBinaryRules JaGetRules
+
+    vector[ScoredNode] ParseSentence(
+            unsigned id,
+            const string& sent,
+            float* tag_scores,
+            float* dep_scores,
+            const unordered_map[string, vector[bool]]& category_dict,
+            const vector[Cat]& tag_list,
+            float beta,
+            bool use_beta,
+            unsigned pruning_size,
+            unsigned nbest,
+            const unordered_set[Cat]& possible_root_cats,
+            const unordered_map[Cat, vector[Cat]]& unary_rules,
+            const vector[Op]& binary_rules,
+            unordered_map[CatPair, vector[RuleCache]]& cache,
+            const unordered_set[CatPair]& seen_rules,
+            ApplyBinaryRules apply_binary_rules,
+            ApplyUnaryRules apply_unary_rules,
+            unsigned max_length) nogil
+
+    vector[vector[ScoredNode]] ParseSentences(
+            vector[string]& sents,
+            float** tag_scores,
+            float** dep_scores,
+            const unordered_map[string, vector[bool]]& category_dict,
+            const vector[Cat]& tag_list,
+            float beta,
+            bint use_beta,
+            unsigned pruning_size,
+            unsigned nbest,
+            const unordered_set[Cat]& possible_root_cats,
+            const unordered_map[Cat, vector[Cat]]& unary_rules,
+            const vector[Op]& binary_rules,
+            unordered_map[CatPair, vector[RuleCache]]& cache,
+            const unordered_set[CatPair]& seen_rules,
+            ApplyBinaryRules apply_binary_rules,
+            ApplyUnaryRules apply_unary_rules,
+            unsigned max_length)
+
+
--- depccg/parser.pyx
+++ depccg/parser.pyx
@@ -1,13 +1,7 @@
 
 from __future__ import print_function
 
-from libcpp.vector cimport vector
-from libcpp.string cimport string
-from libcpp.unordered_set cimport unordered_set
-from libcpp.unordered_map cimport unordered_map
-from libcpp cimport bool
 from libc.stdlib cimport malloc, free
-from cython.parallel cimport prange
 
 cimport numpy as np
 import numpy as np
@@ -19,74 +13,12 @@ import json
 import chainer
 import logging
 
-from .cat cimport Category, Cat, CatPair
-from .tree cimport Tree, ScoredNode
-from .combinator cimport en_headfirst_binary_rules, ja_headfinal_binary_rules, Op
+from .combinator cimport en_headfirst_binary_rules, ja_headfinal_binary_rules
 from .utils cimport *
 from .utils import maybe_split_and_join
 
 
 logger = logging.getLogger(__name__)
-
-
-cdef extern from "depccg.h" namespace "myccg" nogil:
-    cdef cppclass RuleCache
-
-    cdef cppclass AgendaItem
-
-    ctypedef vector[RuleCache]& (*ApplyBinaryRules)(
-            unordered_map[CatPair, vector[RuleCache]]&,
-            const vector[Op]&, const unordered_set[CatPair]&, Cat, Cat)
-
-    ctypedef vector[Cat] (*ApplyUnaryRules)(
-            const unordered_map[Cat, vector[Cat]]&, NodeType)
-
-    ApplyUnaryRules EnApplyUnaryRules
-
-    ApplyUnaryRules JaApplyUnaryRules
-
-    ApplyBinaryRules EnGetRules
-
-    ApplyBinaryRules JaGetRules
-
-    vector[ScoredNode] ParseSentence(
-            unsigned id,
-            const string& sent,
-            float* tag_scores,
-            float* dep_scores,
-            const unordered_map[string, vector[bool]]& category_dict,
-            const vector[Cat]& tag_list,
-            float beta,
-            bool use_beta,
-            unsigned pruning_size,
-            unsigned nbest,
-            const unordered_set[Cat]& possible_root_cats,
-            const unordered_map[Cat, vector[Cat]]& unary_rules,
-            const vector[Op]& binary_rules,
-            unordered_map[CatPair, vector[RuleCache]]& cache,
-            const unordered_set[CatPair]& seen_rules,
-            ApplyBinaryRules apply_binary_rules,
-            ApplyUnaryRules apply_unary_rules,
-            unsigned max_length) nogil
-
-    vector[vector[ScoredNode]] ParseSentences(
-            vector[string]& sents,
-            float** tag_scores,
-            float** dep_scores,
-            const unordered_map[string, vector[bool]]& category_dict,
-            const vector[Cat]& tag_list,
-            float beta,
-            bint use_beta,
-            unsigned pruning_size,
-            unsigned nbest,
-            const unordered_set[Cat]& possible_root_cats,
-            const unordered_map[Cat, vector[Cat]]& unary_rules,
-            const vector[Op]& binary_rules,
-            unordered_map[CatPair, vector[RuleCache]]& cache,
-            const unordered_set[CatPair]& seen_rules,
-            ApplyBinaryRules apply_binary_rules,
-            ApplyUnaryRules apply_unary_rules,
-            unsigned max_length)
 
 
 cdef class EnglishCCGParser:
